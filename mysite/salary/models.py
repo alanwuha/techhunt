67ffd58c6a96f01642087b78d127274d610f4186
Alan Tan <alanwuha91@gmail.com>
2020-05-23 02:40:38 +0800

    removed min_length

--- mysite/salary/models.py
+++ mysite/salary/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 class Employee(models.Model):
-    id = models.CharField(min_length=5, max_length=5, primary_key=True)
+    id = models.CharField(max_length=5, primary_key=True)
     login = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
     salary = models.FloatField(validators=[MinValueValidator(0.0)])
